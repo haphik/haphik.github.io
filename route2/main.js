8529ba07199aeca4b0697f07d46a5b009da45835
fritzcrone <fritz.crone@gmail.com>
2025-06-20 18:09:28 +0200

    script route 1 zu route 2 kopiert

---
+++ route2/main.js
@@ -0,0 +1,60 @@
+/* Route 2 */
+
+// Innsbruck
+let ibk = {
+    lat: 47.267222,
+    lng: 11.392778
+};
+
+// Karte
+let map = L.map("map").setView([ibk.lat, ibk.lng], 9);
+
+// Hintergrundkarten
+let eGrundkarteTirol = {
+    sommer: L.tileLayer("https://wmts.kartetirol.at/gdi_summer/{z}/{x}/{y}.png", {
+        attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
+    }),
+    nomenklatur: L.tileLayer("https://wmts.kartetirol.at/gdi_nomenklatur/{z}/{x}/{y}.png", {
+        attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`,
+        pane: "overlayPane",
+    })
+}
+
+// / Layer control mit eGrundkarte Tirol und Standardlayern
+L.control.layers({
+    "eGrundkarte Tirol Sommer": L.layerGroup([
+        eGrundkarteTirol.sommer,
+        eGrundkarteTirol.nomenklatur
+    ]).addTo(map),
+
+    "OpenStreetMap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "EsriWorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
+    "BasemapAT grau": L.tileLayer.provider("BasemapAT.grau"),
+}).addTo(map);
+
+// Maßstab
+L.control.scale({
+    imperial: false,
+}).addTo(map);
+
+
+// leaflet plugin elevation
+var controlElevation = L.control.elevation({
+    theme: "hike",
+    time:false,
+    elevationDiv: "#profile",
+    height: 300,
+
+    //slope: true,
+}).addTo(map);
+controlElevation.load("Salfeinsee.gpx")
+
+//Minimap
+var wmts = new L.TileLayer("https://wmts.kartetirol.at/gdi_summer/{z}/{x}/{y}.png", {minZoom: 0, maxZoom: 13});
+var miniMap = new L.Control.MiniMap(wmts, {
+    toggleDisplay: true,
+    minimized: true,
+}).addTo(map);
+
+//Fullscreen
+map.addControl(new L.Control.Fullscreen());
